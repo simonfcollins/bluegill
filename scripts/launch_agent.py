eb1c5c0bdabc393c1000acc5e1b113cb8b301b13
simonfcollins <simonfcollins05@gmail.com>
2026-06-10 10:36:08 -0700

    changed default workspace behavior

--- scripts/launch_agent.py
+++ scripts/launch_agent.py
@@ -1,26 +1,37 @@
 import subprocess
 from pathlib import Path
-from bluegill_shared.utils import *
+from bluegill_shared.utils import Config, load_config
+
 
 BASE_DIR = Path.home() / ".bluegill"
 BASE_DIR.mkdir(exist_ok=True)
 
-DEFAULT_WORKSPACE = BASE_DIR / "workspace"
+# reference to the default agent workspace on the host
+DEFAULT_WORKSPACE = BASE_DIR / "default-workspace"
 
 DOCKER_USER = "assistant"
 
+# reference to the base workspaces directory in the Docker container
 DOCKER_BASE_DIR = Path(f"/home/{DOCKER_USER}/workspaces/")
 
 DOCKER_IMAGE = "bluegill_agent:latest"
 
 
 def load_workspace_dirs() -> list[Path]:
+    """
+    Reads the config.json file and returns the list of workspace paths.
+    """
+
     cfg: Config = load_config()
         
     return [Path(ws.path).expanduser().resolve() for ws in cfg.workspaces]
 
 
-def verify_docker():
+def verify_docker() -> bool:
+    """
+    Returns True if Docker is installed on the host. False otherwise.
+    """
+
     try:
         result = subprocess.run(
             [
@@ -35,7 +46,13 @@ def verify_docker():
     except subprocess.CalledProcessError:
         return False
 
-def verify_image():
+
+def verify_image() -> bool:
+    """
+    Returns True if the bluegill_agent:latest Docker image exists.
+    False otherwise.
+    """
+
     try:
         result = subprocess.run(
             [
@@ -52,7 +69,13 @@ def verify_image():
     except subprocess.CalledProcessError:
         return False
     
-def pull_docker_image():
+
+def pull_docker_image() -> str:
+    """
+    Retrieves the latest version of the bluegill_agent Docker image.
+    """
+
+    # pull the latest version of the bluegill_agent image
     result = subprocess.run(
         ["docker", "pull", "ghcr.io/simonfcollins/bluegill_agent:latest"],
         stdout=subprocess.PIPE,
@@ -63,6 +86,7 @@ def pull_docker_image():
     if result.returncode != 0:
         raise RuntimeError(f"Failed to pull image:\n{result.stderr}")
     
+    # rename the image
     subprocess.run(
     [
         "docker", "tag",
@@ -74,11 +98,20 @@ def pull_docker_image():
 
     return result.stdout
 
-def launch_agent():
+
+def launch_agent() -> None:
+    """
+    Launch a Docker container from the bluegill_agent:latest image. 
+    Verifies that Docker is installed, pulls the image, and defines bind mounts
+    from the config.json file.
+    """
+
+    # check if Docker is installed
     if not verify_docker():
         print("Error: dependency \'docker\' is not installed")
         return
     
+    # pull the bluegill_agent Docker image if it doesn't exist locally
     if not verify_image():
         try:
             pull_docker_image()
@@ -86,20 +119,25 @@ def launch_agent():
             print(e)
             return
         
+    
+    # if the image still doesn't exist, exit
     if not verify_image():
         print(f"Error: unable to retrieve image \'{DOCKER_IMAGE}\'")
         return
         
+    # prepare the workspaces for bind mounting
     mounts = []
     workspace_dirs = load_workspace_dirs()
     
-    if len(workspace_dirs) == 0:
-        DEFAULT_WORKSPACE.mkdir(exist_ok=True)
-        mounts.extend(["-v", f"{DEFAULT_WORKSPACE}:{DOCKER_BASE_DIR / 'workspace'}"])
-    else:
-        for d in workspace_dirs:
-            mounts.extend(["-v", f"{Path(d).expanduser().resolve()}:{DOCKER_BASE_DIR / Path(d).name}"])
+    # default workspace
+    DEFAULT_WORKSPACE.mkdir(exist_ok=True)
+    mounts.extend(["-v", f"{DEFAULT_WORKSPACE}:{DOCKER_BASE_DIR / 'default-workspace'}"])
+
+    # user-defined workspaces
+    for d in workspace_dirs:
+        mounts.extend(["-v", f"{Path(d).expanduser().resolve()}:{DOCKER_BASE_DIR / Path(d).name}"])
         
+    # docker launch command
     cmd = [
         "docker", "run", "--rm",
         "--network=host",
